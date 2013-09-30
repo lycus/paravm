b305da18ada0040136740a138615ea6a9ca2da83
alexrp <alex@lycus.org>
2013-10-01 00:22:05 +0200

    Overhaul the opcode table with updated definitions.
    Also fix some broken lookup code.

--- paravm/include/opcodes.h
+++ paravm/include/opcodes.h
@@ -2,69 +2,107 @@
 
 #include "common.h"
 
-typedef enum {
-    PARAVM_OPERAND_TYPE_NONE = 0,
-    PARAVM_OPERAND_TYPE_INTEGER = 1,
-    PARAVM_OPERAND_TYPE_FLOAT = 2,
-    PARAVM_OPERAND_TYPE_ATOM = 3,
-    PARAVM_OPERAND_TYPE_STRING = 4,
-    PARAVM_OPERAND_TYPE_BITS = 5,
-    PARAVM_OPERAND_TYPE_ARGS = 6,
-    PARAVM_OPERAND_TYPE_GLOBAL = 7,
-    PARAVM_OPERAND_TYPE_BLOCK = 8,
-} ParaVMOperandType;
+paravm_begin
 
-typedef struct {
-    const char *name;
-    uint8_t code;
-    uint8_t registers;
-    ParaVMOperandType operand;
-} ParaVMOpCode;
+typedef enum ParaVMOperandType ParaVMOperandType;
 
-extern const ParaVMOpCode paravm_op_call;
-extern const ParaVMOpCode paravm_op_ret;
-extern const ParaVMOpCode paravm_op_ldi;
-extern const ParaVMOpCode paravm_op_ldf;
-extern const ParaVMOpCode paravm_op_lda;
-extern const ParaVMOpCode paravm_op_lds;
-extern const ParaVMOpCode paravm_op_ldb;
-extern const ParaVMOpCode paravm_op_mov;
-extern const ParaVMOpCode paravm_op_add;
-extern const ParaVMOpCode paravm_op_sub;
-extern const ParaVMOpCode paravm_op_mul;
-extern const ParaVMOpCode paravm_op_div;
-extern const ParaVMOpCode paravm_op_rem;
-extern const ParaVMOpCode paravm_op_neg;
-extern const ParaVMOpCode paravm_op_and;
-extern const ParaVMOpCode paravm_op_or;
-extern const ParaVMOpCode paravm_op_xor;
-extern const ParaVMOpCode paravm_op_not;
-extern const ParaVMOpCode paravm_op_shl;
-extern const ParaVMOpCode paravm_op_shr;
-extern const ParaVMOpCode paravm_op_stg;
-extern const ParaVMOpCode paravm_op_ldg;
-extern const ParaVMOpCode paravm_op_jmp;
-extern const ParaVMOpCode paravm_op_jmpc;
-extern const ParaVMOpCode paravm_op_clt;
-extern const ParaVMOpCode paravm_op_cgt;
-extern const ParaVMOpCode paravm_op_cne;
-extern const ParaVMOpCode paravm_op_ceq;
-extern const ParaVMOpCode paravm_op_cge;
-extern const ParaVMOpCode paravm_op_cle;
+/* Specifies the kind of operand that an opcode expects.
+ */
+enum ParaVMOperandType
+{
+    PARAVM_OPERAND_TYPE_NONE = 0, // No operand.
+    PARAVM_OPERAND_TYPE_INTEGER = 1, // An integer literal.
+    PARAVM_OPERAND_TYPE_FLOAT = 2, // A floating point literal.
+    PARAVM_OPERAND_TYPE_ATOM = 3, // An atom literal.
+    PARAVM_OPERAND_TYPE_BITS = 4, // A bit vector or byte array.
+    PARAVM_OPERAND_TYPE_ARGS = 5, // An argument list.
+    PARAVM_OPERAND_TYPE_BLOCK = 6, // A basic block.
+    PARAVM_OPERAND_TYPE_BLOCKS = 7, // Two basic blocks.
+};
 
+typedef enum ParaVMControlFlow ParaVMControlFlow;
+
+/* Specifies the control flow effects of an opcode.
+ */
+enum ParaVMControlFlow
+{
+    PARAVM_CONTROL_FLOW_NONE = 0, // No control flow.
+    PARAVM_CONTROL_FLOW_BRANCH = 1, // Branches to another basic block.
+    PARAVM_CONTROL_FLOW_RETURN = 2, // Exits the current function.
+    PARAVM_CONTROL_FLOW_THROW = 3, // Throws an exception.
+};
+
+typedef struct ParaVMOpCode ParaVMOpCode;
+
+/* Defines the structure of an opcode.
+ */
+struct ParaVMOpCode
+{
+    const char *name; // The name of the opcode.
+    uint8_t code; // The byte code of the opcode.
+    uint8_t registers; // The number of registers the opcode operates on.
+    ParaVMOperandType operand; // The kind of operand the opcode expects.
+    ParaVMControlFlow control_flow; // The control flow effect of the opcode.
+};
+
+extern const ParaVMOpCode paravm_op_app; // The `app` opcode.
+extern const ParaVMOpCode paravm_op_ret; // The `ret` opcode.
+extern const ParaVMOpCode paravm_op_ldi; // The `ldi` opcode.
+extern const ParaVMOpCode paravm_op_ldf; // The `ldf` opcode.
+extern const ParaVMOpCode paravm_op_lda; // The `lda` opcode.
+extern const ParaVMOpCode paravm_op_ldb; // The `ldb` opcode.
+extern const ParaVMOpCode paravm_op_mov; // The `mov` opcode.
+extern const ParaVMOpCode paravm_op_add; // The `add` opcode.
+extern const ParaVMOpCode paravm_op_sub; // The `sub` opcode.
+extern const ParaVMOpCode paravm_op_mul; // The `mul` opcode.
+extern const ParaVMOpCode paravm_op_div; // The `div` opcode.
+extern const ParaVMOpCode paravm_op_rem; // The `rem` opcode.
+extern const ParaVMOpCode paravm_op_pow; // The `pow` opcode.
+extern const ParaVMOpCode paravm_op_neg; // The `neg` opcode.
+extern const ParaVMOpCode paravm_op_and; // The `and` opcode.
+extern const ParaVMOpCode paravm_op_or; // The `or` opcode.
+extern const ParaVMOpCode paravm_op_xor; // The `xor` opcode.
+extern const ParaVMOpCode paravm_op_not; // The `not` opcode.
+extern const ParaVMOpCode paravm_op_shl; // The `shl` opcode.
+extern const ParaVMOpCode paravm_op_shr; // The `shr` opcode.
+extern const ParaVMOpCode paravm_op_br; // The `br` opcode.
+extern const ParaVMOpCode paravm_op_brc; // The `brc` opcode.
+extern const ParaVMOpCode paravm_op_clt; // The `clt` opcode.
+extern const ParaVMOpCode paravm_op_cgt; // The `cgt` opcode.
+extern const ParaVMOpCode paravm_op_cne; // The `cne` opcode.
+extern const ParaVMOpCode paravm_op_ceq; // The `ceq` opcode.
+extern const ParaVMOpCode paravm_op_cge; // The `cge` opcode.
+extern const ParaVMOpCode paravm_op_cle; // The `cle` opcode.
+extern const ParaVMOpCode paravm_op_upc; // The `upc` opcode.
+extern const ParaVMOpCode paravm_op_eht; // The `eht` opcode.
+extern const ParaVMOpCode paravm_op_ehc; // The `ehc` opcode.
+extern const ParaVMOpCode paravm_op_ehr; // The `ehr` opcode.
+extern const ParaVMOpCode paravm_op_tcr; // The `tcr` opcode.
+extern const ParaVMOpCode paravm_op_nop; // The `nop` opcode.
+
+/* Gets a list of pointers to all opcodes. The list is
+ * `NULL`-terminated.
+ */
 paravm_api
 paravm_nothrow
-paravm_pure
 paravm_nonnull()
 const ParaVMOpCode *const *paravm_get_opcodes(void);
 
+/* Gets an opcode with a name equal to `name`.
+ *
+ * Returns the matching opcode or `NULL`.
+ */
 paravm_api
 paravm_nothrow
-paravm_pure
 paravm_nonnull()
 const ParaVMOpCode *paravm_get_opcode_by_name(const char *name);
 
+/* Gets an opcode with a byte code equal to `code`.
+ *
+ * Returns the matching opcode or `NULL`.
+ */
 paravm_api
 paravm_nothrow
-paravm_pure
 const ParaVMOpCode *paravm_get_opcode_by_code(uint8_t code);
+
+paravm_end
--- paravm/src/opcodes.c
+++ paravm/src/opcodes.c
@@ -2,45 +2,48 @@
 
 #include "opcodes.h"
 
-const ParaVMOpCode paravm_op_call = { "call", 0x00, 3, PARAVM_OPERAND_TYPE_ARGS };
-const ParaVMOpCode paravm_op_ret = { "ret", 0x01, 1, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_ldi = { "ldi", 0x02, 1, PARAVM_OPERAND_TYPE_INTEGER };
-const ParaVMOpCode paravm_op_ldf = { "ldf", 0x03, 1, PARAVM_OPERAND_TYPE_FLOAT };
-const ParaVMOpCode paravm_op_lda = { "lda", 0x04, 1, PARAVM_OPERAND_TYPE_ATOM };
-const ParaVMOpCode paravm_op_lds = { "lds", 0x05, 1, PARAVM_OPERAND_TYPE_STRING };
-const ParaVMOpCode paravm_op_ldb = { "ldb", 0x06, 1, PARAVM_OPERAND_TYPE_BITS };
-const ParaVMOpCode paravm_op_mov = { "mov", 0x07, 2, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_add = { "add", 0x08, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_sub = { "sub", 0x09, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_mul = { "mul", 0x0A, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_div = { "div", 0x0B, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_rem = { "rem", 0x0C, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_neg = { "neg", 0x0D, 2, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_and = { "and", 0x0E, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_or = { "or", 0x0F, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_xor = { "xor", 0x10, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_not = { "not", 0x11, 2, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_shl = { "shl", 0x12, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_shr = { "shr", 0x13, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_stg = { "stg", 0x14, 1, PARAVM_OPERAND_TYPE_GLOBAL };
-const ParaVMOpCode paravm_op_ldg = { "ldg", 0x15, 1, PARAVM_OPERAND_TYPE_GLOBAL };
-const ParaVMOpCode paravm_op_jmp = { "jmp", 0x16, 0, PARAVM_OPERAND_TYPE_BLOCK };
-const ParaVMOpCode paravm_op_jmpc = { "jmpc", 0x17, 1, PARAVM_OPERAND_TYPE_BLOCK };
-const ParaVMOpCode paravm_op_clt = { "clt", 0x18, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_cgt = { "cgt", 0x19, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_cne = { "cne", 0x1A, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_ceq = { "ceq", 0x1B, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_cge = { "cge", 0x1C, 3, PARAVM_OPERAND_TYPE_NONE };
-const ParaVMOpCode paravm_op_cle = { "cle", 0x1D, 3, PARAVM_OPERAND_TYPE_NONE };
+const ParaVMOpCode paravm_op_app = { "app", 0x00, 3, PARAVM_OPERAND_TYPE_ARGS, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_ret = { "ret", 0x01, 1, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_RETURN };
+const ParaVMOpCode paravm_op_ldi = { "ldi", 0x02, 1, PARAVM_OPERAND_TYPE_INTEGER, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_ldf = { "ldf", 0x03, 1, PARAVM_OPERAND_TYPE_FLOAT, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_lda = { "lda", 0x04, 1, PARAVM_OPERAND_TYPE_ATOM, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_ldb = { "ldb", 0x05, 1, PARAVM_OPERAND_TYPE_BITS, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_mov = { "mov", 0x06, 2, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_add = { "add", 0x07, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_sub = { "sub", 0x08, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_mul = { "mul", 0x09, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_div = { "div", 0x0A, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_rem = { "rem", 0x0B, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_pow = { "pow", 0x0C, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_neg = { "neg", 0x0D, 2, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_and = { "and", 0x0E, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_or = { "or", 0x0F, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_xor = { "xor", 0x10, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_not = { "not", 0x11, 2, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_shl = { "shl", 0x12, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_shr = { "shr", 0x13, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_br = { "br", 0x14, 0, PARAVM_OPERAND_TYPE_BLOCK, PARAVM_CONTROL_FLOW_BRANCH };
+const ParaVMOpCode paravm_op_brc = { "brc", 0x15, 1, PARAVM_OPERAND_TYPE_BLOCKS, PARAVM_CONTROL_FLOW_BRANCH };
+const ParaVMOpCode paravm_op_clt = { "clt", 0x16, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_cgt = { "cgt", 0x17, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_cne = { "cne", 0x18, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_ceq = { "ceq", 0x19, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_cge = { "cge", 0x1A, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_cle = { "cle", 0x1B, 3, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_upc = { "upc", 0x1C, 3, PARAVM_OPERAND_TYPE_ARGS, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_eht = { "eht", 0x1D, 1, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_THROW };
+const ParaVMOpCode paravm_op_ehc = { "ehc", 0x1E, 1, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
+const ParaVMOpCode paravm_op_ehr = { "ehr", 0x1F, 0, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_THROW };
+const ParaVMOpCode paravm_op_tcr = { "tcr", 0x20, 2, PARAVM_OPERAND_TYPE_ARGS, PARAVM_CONTROL_FLOW_RETURN };
+const ParaVMOpCode paravm_op_nop = { "nop", 0x21, 0, PARAVM_OPERAND_TYPE_NONE, PARAVM_CONTROL_FLOW_NONE };
 
 static const ParaVMOpCode *opcodes[] =
 {
-    &paravm_op_call,
+    &paravm_op_app,
     &paravm_op_ret,
     &paravm_op_ldi,
     &paravm_op_ldf,
     &paravm_op_lda,
-    &paravm_op_lds,
     &paravm_op_ldb,
     &paravm_op_mov,
     &paravm_op_add,
@@ -55,16 +58,20 @@ static const ParaVMOpCode *opcodes[] =
     &paravm_op_not,
     &paravm_op_shl,
     &paravm_op_shr,
-    &paravm_op_stg,
-    &paravm_op_ldg,
-    &paravm_op_jmp,
-    &paravm_op_jmpc,
+    &paravm_op_br,
+    &paravm_op_brc,
     &paravm_op_clt,
     &paravm_op_cgt,
     &paravm_op_cne,
     &paravm_op_ceq,
     &paravm_op_cge,
     &paravm_op_cle,
+    &paravm_op_upc,
+    &paravm_op_eht,
+    &paravm_op_ehc,
+    &paravm_op_ehr,
+    &paravm_op_tcr,
+    &paravm_op_nop,
     null,
 };
 
@@ -72,23 +79,24 @@ static GHashTable *opcode_name_table;
 static GHashTable *opcode_code_table;
 
 global_ctor
-static void global_opcodes_ctor(void)
+static void global_opcode_ctor(void)
 {
     opcode_name_table = g_hash_table_new(&g_str_hash, &g_str_equal);
-    opcode_code_table = g_hash_table_new(&g_int_hash, &g_int_equal);
+    opcode_code_table = g_hash_table_new_full(&g_int_hash, &g_int_equal, &g_free, null);
 
     for (const ParaVMOpCode **opc = &opcodes[0]; *opc; opc++)
     {
-        const char *name = (*opc)->name;
-        uint8_t code = (*opc)->code;
+        g_hash_table_insert(opcode_name_table, (gpointer)(*opc)->name, (gpointer)*opc);
 
-        g_hash_table_insert(opcode_name_table, &name, opc);
-        g_hash_table_insert(opcode_code_table, &code, opc);
+        uint32_t *p = g_new(uint32_t, 1);
+        *p = (*opc)->code;
+
+        g_hash_table_insert(opcode_code_table, p, (gpointer)*opc);
     }
 }
 
 global_dtor
-static void global_opcodes_dtor(void)
+static void global_opcode_dtor(void)
 {
     g_hash_table_destroy(opcode_name_table);
     g_hash_table_destroy(opcode_code_table);
@@ -101,10 +109,14 @@ const ParaVMOpCode *const *paravm_get_opcodes(void)
 
 const ParaVMOpCode *paravm_get_opcode_by_name(const char *name)
 {
-    return g_hash_table_lookup(opcode_name_table, &name);
+    assert(name);
+
+    return g_hash_table_lookup(opcode_name_table, name);
 }
 
 const ParaVMOpCode *paravm_get_opcode_by_code(uint8_t code)
 {
-    return g_hash_table_lookup(opcode_code_table, &code);
+    int c = code;
+
+    return g_hash_table_lookup(opcode_code_table, &c);
 }
