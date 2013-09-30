9b8d14083a2fea002ed4c63faf0cfc0050c5d12b
alexrp <alex@lycus.org>
2013-10-01 00:22:51 +0200

    Better assert definitions. Add paravm_assert_unreachable.

--- paravm/include/internal/common.h
+++ paravm/include/internal/common.h
@@ -22,13 +22,20 @@
 #define _STRINGIFY(X) #X
 #define STRINGIFY(X) _STRINGIFY(X)
 
-void paravm_assert(bool cond, const char *file, int line,
-                   const char *func, const char *msg) paravm_internal;
+paravm_internal
+paravm_nothrow
+paravm_nonnull()
+noreturn
+void paravm_assert(const char *file, int line, const char *func, const char *msg);
 
-#ifdef PARAVM_DEBUG
-#    define assert(CND) paravm_assert(!!(CND), __FILE__, __LINE__, __func__, STRINGIFY(CND))
+#if defined(PARAVM_DEBUG)
+#    define assert(CND) \
+        if (!expect(!!(CND))) \
+            paravm_assert(__FILE__, __LINE__, __func__, STRINGIFY(CND))
+#    define assert_unreachable() paravm_assert(__FILE__, __LINE__, __func__, "should not be reached")
 #else
 #    define assert(CND)
+#    define assert_unreachable()
 #endif
 
 #define static_assert(CND) _Static_assert(CND, STRINGIFY(CND))
--- paravm/src/common.c
+++ paravm/src/common.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void paravm_assert(bool cond, const char *file, int line,
-                   const char *func, const char *msg)
+void paravm_assert(const char *file, int line, const char *func, const char *msg)
 {
-    if (!expect(cond)) \
-    {
-        printf("Assertion failed at %s:%i!%s: %s\n", file, line, func, msg);
-        abort();
-    }
+    printf("Assertion failed at %s:%i!%s: %s\n", file, line, func, msg);
+    abort();
 }
